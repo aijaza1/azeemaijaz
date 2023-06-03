b5e2121b9a2272a82eae240003a1b13b1c9c1a31
Azeem <aijaza@vcu.edu>
2023-06-03 17:20:06 -0400

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-}
-
 module.exports = {
     images: {
-        unoptimized: true
-    }
-}
+      unoptimized: true
+    },
+    async generateStaticPaths() {
+      return [
+        { params: { slug: 'main' }, locale: 'en' },
+        { params: { slug: 'about' }, locale: 'en' },
+        { params: { slug: 'layout' }, locale: 'en' },
+        { params: { slug: 'page' }, locale: 'en' },
+        { params: { slug: 'contact' }, locale: 'en' },
+        { params: { slug: 'Navbar' }, locale: 'en' },
+        { params: { slug: 'ProjectItem' }, locale: 'en' },
+        { params: { slug: 'projects' }, locale: 'en' },
+        { params: { slug: 'skills' }, locale: 'en' },
+        
+        // Add more static paths as needed
+      ];
+    },
+  };
