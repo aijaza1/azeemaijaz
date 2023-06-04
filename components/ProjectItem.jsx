81972287553067700f61effca605db7627535b12
Azeem <aijaza@vcu.edu>
2023-06-03 21:48:36 -0400

    test

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const ProjectItem = ({title, backgroundImg, tools, projectUrl}) => {
+    
+
   return (
         <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651E5] to-[#709DFF]'>
                         
@@ -13,7 +15,7 @@ const ProjectItem = ({title, backgroundImg, tools, projectUrl}) => {
             <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
             <p className='pb-4 pt-2 text-white text-center'>{tools}</p>
             
-            <Link href={"/projectUrl"}>
+            <Link href="/noblecanni">
                 <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
             </Link>
         </div>
