4a0728628f11ff6cd5eedf4a891ceb03fe7ef211
Azeem <aijaza@vcu.edu>
2023-06-04 20:47:01 -0400

    update

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -15,7 +15,7 @@ const ProjectItem = ({title, backgroundImg, tools, projectUrl}) => {
             <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
             <p className='pb-4 pt-2 text-white text-center'>{tools}</p>
             
-            <Link href="/capstone">
+            <Link href={projectUrl}>
                 <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
             </Link>
         </div>
