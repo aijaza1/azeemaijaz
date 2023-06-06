9ed74b5aa2b658e3c7a77e297d518b1a9b2d4c11
Azeem <aijaza@vcu.edu>
2023-06-05 21:40:16 -0400

    test

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -117,7 +117,7 @@ const Navbar = () => {
           ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' 
           : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
             
-            <div className='md:hidden '>
+            <div className='sm:hidden '>
               <div className='flex w-full items-center justify-between'>
                 
                 <Link href='/'>
