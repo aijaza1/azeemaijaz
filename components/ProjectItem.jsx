fbf8d6efe7133c3a7c274eb03d05bb3c40d8a6ba
Azeem <aijaza@vcu.edu>
2023-07-19 13:43:17 -0400

    fix

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -10,11 +10,8 @@ const ProjectItem = ({title, backgroundImg, tech, projectUrl}) => {
         <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
         <p className='pb-4 pt-2 text-white text-center'>{tech}</p>
         <p className='pb-4 pt-2 text-white text-center'>{projectUrl}</p>
-        <Link href={projectUrl}>
-          <a className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</a>
-        </Link>
         <Link href={projectUrl} passHref>
-          <a className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</a>
+          <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
         </Link>
       
     </div>
