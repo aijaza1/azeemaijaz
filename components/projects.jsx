b2f1844dacb8f587af2ed50954c34b981d599a03
Azeem <aijaza@vcu.edu>
2023-07-19 16:58:19 -0400

    commit

--- components/projects.jsx
+++ components/projects.jsx
@@ -23,6 +23,7 @@ const Projects = () => {
         <h2 className='py-4'>What I&apos;ve Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
           
+          
           <ProjectItem
             title='NobleCanni Web Dev Intern'
             backgroundImg={nobleImg}
