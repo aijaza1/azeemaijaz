7e2c7b383c2c38a433e3e3bd909c6da3240650cb
Azeem <aijaza@vcu.edu>
2023-07-19 13:35:43 -0400

    test

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -11,7 +11,7 @@ const ProjectItem = ({title, backgroundImg, tech, projectUrl}) => {
         <p className='pb-4 pt-2 text-white text-center'>{tech}</p>
         <p className='pb-4 pt-2 text-white text-center'>{projectUrl}</p>
         <Link href={projectUrl}>
-            <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
+          <a className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</a>
         </Link>
         <Link to={{ pathname: {projectUrl} }} target="_blank">Click me</Link>
         <Link to={{ pathname: projectUrl }} target="_blank">Click me</Link>
