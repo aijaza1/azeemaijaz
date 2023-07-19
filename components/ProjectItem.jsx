1de3f46bf73f5ae8b3c1af58ec198bc6b61e9c87
Azeem <aijaza@vcu.edu>
2023-07-19 13:38:18 -0400

    work

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -13,6 +13,9 @@ const ProjectItem = ({title, backgroundImg, tech, projectUrl}) => {
         <Link href={projectUrl}>
           <a className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</a>
         </Link>
+        <Link href={projectUrl} passHref>
+          <a className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</a>
+        </Link>
         <Link to={{ pathname: {projectUrl} }} target="_blank">Click me</Link>
         <Link to={{ pathname: projectUrl }} target="_blank">Click me</Link>
     </div>
